36b381fafadff628138983cfbec32f4ab71bb3b5
alexyy0 <1970912595@qq.com>
2019-06-28 00:57:43 +0800

    PasswordManager

---
+++ src/JTextFieldLimit/PasswordLimit.java
@@ -0,0 +1,32 @@
+package JTextFieldLimit;
+
+import javax.swing.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordLimit extends PlainDocument {
+    private int limit;  //限制的长度
+
+    public PasswordLimit(int limit) {
+        super(); //调用父类构造
+        this.limit = limit;
+    }
+    public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
+        Pattern num = Pattern.compile("[0-9]*");
+        Matcher n = num.matcher(str);
+        Pattern letter=Pattern.compile("[a-zA-Z]");
+        Matcher l = letter.matcher(str);
+        if(str == null)
+            return;
+
+        if((n.matches()||l.matches())&&(getLength() + str.length()) <= limit){
+            super.insertString(offset, str, attr);//调用父类方法
+        }else {
+            JOptionPane.showMessageDialog(null, "最多输入11位，且只能输入字母,数字！", "注意", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+}
